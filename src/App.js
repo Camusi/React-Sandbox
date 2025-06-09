6e36a30553aa4fc5356025487775179afdabed23
Sammy <scu39@uclive.ac.nz>
2025-06-09 19:31:23 +1200

    Added home and BeginnersArchive pages also navbar

--- src/App.js
+++ src/App.js
@@ -1,57 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-const user = {
-    name: 'Hedy Lamarr',
-    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-    imageSize: 90,
-};
-
-const products = [
-    { title: 'Cabbage', isFruit: false, id: 1 },
-    { title: 'Garlic', isFruit: false, id: 2 },
-    { title: 'Apple', isFruit: true, id: 3 },
-];
-
-const listItems = products.map(product =>
-    <li
-        key={product.id}
-        style={{
-            color: product.isFruit ? 'magenta' : 'darkgreen'
-        }}
-    >
-        {product.title}
-    </li>
-);
-
-let clicked = false;
+import NavBar from './NavBar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import BeginnersArchive from './BeginnersArchive';
+import {useState} from "react";
 
 export default function App() {
+    const [gold, setGold] = useState(0)
     return (
         <div>
-            <h1>{clicked ? "Welcome to my app" : "Why haven't you clicked it yet?"}</h1>
-            <MyButton /><br />
-            <img className="avatar" src={logo} alt="logo"/>
-
-            <h1>{user.name}</h1>
-            <img
-              className="avatar"
-              src={user.imageUrl}
-              alt={'Photo of ' + user.name}
-              style={{
-                  width: user.imageSize,
-                  height: user.imageSize
-              }}
-            />
-            <ul>{listItems}</ul>
+            <div style={{ height: '5rem', backgroundColor: "#fff9e6" }}></div>
+            <NavBar gold={gold}/>
+            <Routes>
+                <Route path="/" element={<Home setGold={setGold}/>} />
+                <Route path="/beginners-archive" element={<BeginnersArchive />} />
+            </Routes>
         </div>
-    );
-}
-
-
-function MyButton() {
-    let button_text = "Click me!";
-    return (
-      <button className="test_button">{button_text}</button>
     );
 }
